2f5dfaa8d6a5711fb713b2ed3a2cb916493980cb
Juan Laurencio <juanlaurencio1105@gmail.com>
2023-08-01 13:35:31 -0500

    Nuevas Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ import Restaurant from './pages/Restaurant'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import Turism from './pages/Turism'
-import '/public/css/app.css'
-import { Route, Routes } from 'react-router-dom'
 import RestaurantDetail from './components/restaurant/RestaurantDetail'
+import HotelDetail from './components/hotel/HotelDetail'
+import HistoryHco from './pages/HistoryHco'
+import { Route, Routes } from 'react-router-dom'
+import '/public/css/app.css'
 
 function App() {
 
@@ -18,9 +20,11 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route index path='/' element={<Home />} />
+        <Route path='/huamachuco' element={<HistoryHco />} />
         <Route path='/turismo' element={<Turism />} />
         <Route path='/turismo/:slug' element={<ToursDetail />} />
+        <Route path='/hoteles/:slug' element={<HotelDetail />} />
         <Route path='/costumbre/:slug' element={<CustomDetail />} />
         <Route path='/nosotros' element={<AboutUs />} />
         <Route path='/restaurantes' element={<Restaurant />} />
