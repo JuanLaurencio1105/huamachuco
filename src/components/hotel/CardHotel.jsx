28bdeb922e8fc5e308242a965fa0e8db85c0ffe7
Juan Laurencio <juanlaurencio1105@gmail.com>
2023-08-01 13:34:23 -0500

    Add new page

---
+++ src/components/hotel/CardHotel.jsx
@@ -0,0 +1,63 @@
+import { BiMap } from 'react-icons/bi'
+import { BsCheck2 } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+import Button from '../Button'
+import { FiPhone } from 'react-icons/fi'
+
+const CardHotel = ({ data, onClick }) => {
+
+  const scrollTop = () => {
+    window.scrollTo(0, 0)
+  }
+
+  return (
+    <article className='border border-slate-400 w-full bg-white overflow-hidden flex flex-col mt-6 sm:flex-row md:flex-col lg:flex-row xl:flex-col'>
+      <div className='overflow-hidden'>
+        <Link
+          to={`/hoteles/${data.slug}`}
+          onClick={onClick}
+        >
+          <img
+            src={data.image}
+            alt="Imagen del Hotel"
+            className='w-full h-48 bg-cover bg-center object-cover sm:h-full max-h-60 xl:h-44 overflow-hidden hover:scale-110 transition-all'
+          />
+        </Link>
+      </div>
+      <div className='flex flex-col justify-center items-center gap-2 w-full lg:gap-4'>
+        <h5>{data.title}</h5>
+        <ul className='px-4'>
+          <li className='inline-block bg-green-300 px-2 rounded-md mb-2'>
+            <BsCheck2 className='inline-block mr-1' />
+            <span className='inline-block'>{data.recomend}</span>
+          </li>
+          <li className='flex items-center mb-2 gap-1'>
+            <span>
+              <BiMap size={20} />
+            </span>
+            <p>{data.address}</p>
+          </li>
+          {data.phone !== "" && (
+            <li className='flex items-center gap-1'>
+              <span>
+                <FiPhone size={20} />
+              </span>
+              <p>{data.phone}</p>
+            </li>
+          )}
+        </ul>
+        <div className='flex justify-center mb-2'>
+          <Button
+            type='primary'
+            onClick={scrollTop}
+            to={`/hoteles/${data.slug}`}
+          >
+            Ver Mas
+          </Button>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default CardHotel
---
+++ src/components/hotel/HotelDetail.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import datosJson from '../../../public/datos.json'
+import Head from '../Head'
+import Layout from '../../layouts/Layout'
+import { useParams } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules'
+import { TbHotelService } from 'react-icons/tb'
+import { BiMap } from 'react-icons/bi'
+import { FiPhone } from 'react-icons/fi'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/autoplay'
+import 'swiper/css/effect-fade'
+import 'swiper/css/effect-cards'
+
+const HotelDetail = () => {
+
+  const { slug } = useParams()
+  const [data] = useState(datosJson.Hoteles)
+  const hoteles = data.find((hotel) => hotel.slug === slug)
+
+  return (
+    <>
+      <Head
+        image={hoteles?.hero}
+        title={hoteles.title}
+      />
+      <Layout>
+        <div className='mt-6'>
+          <div className='flex flex-col items-center gap-4 border border-slate-400 xl:flex-row xl:max-h-96'>
+            <Swiper
+              className='w-full max-w-xl'
+              grabCursor={true}
+              effect={'fade'}
+              spaceBetween={10}
+              slidesPerView={1}
+              loop='true'
+              modules={[Navigation, Pagination, Autoplay, EffectFade]}
+              autoplay={{ delay: 4000, disableOnInteraction: false }}
+              pagination={{ clickable: true }}
+              navigation={{ clickable: true }}
+            >
+              {hoteles.gallery.map((image, index) => (
+                <SwiperSlide key={index + 1}>
+                  <img
+                    src={image || ''}
+                    alt={hoteles?.title}
+                    className='w-full h-80 object-cover bg-center bg-cover md:h-80 lg:h-96'
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <div className='w-full flex flex-col gap-4 px-4'>
+              <h2>{hoteles.title}</h2>
+              <div className='flex flex-col gap-4'>
+                <p className='flex items-center gap-3'>
+                  <BiMap size={20} />
+                  {hoteles.address}
+                </p>
+                <p className='flex items-center gap-3'>
+                  <FiPhone size={20} />
+                  {hoteles.phone}
+                </p>
+              </div>
+              <div className='flex flex-col gap-4 pb-4'>
+                <h2>Servicios del establecimiento</h2>
+                <ul className='grid grid-cols-1 gap-4 sm:grid-cols-2 items-center justify-center'>
+                  {hoteles.services.map((service, index) => (
+                    <li key={index + 1} className='flex items-center gap-3'>
+                      <TbHotelService size={20} />
+                      {service}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  )
+}
+
+export default HotelDetail
